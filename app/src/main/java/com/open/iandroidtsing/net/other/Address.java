47fea60b266535cc6c5a088cf3ae0a943bec489e
Long <longhui.yang@inveno.cn>
2017-11-17 17:05:03 +0800

    New changelist

---
+++ app/src/main/java/com/open/iandroidtsing/net/other/Address.java
@@ -0,0 +1,15 @@
+package com.open.iandroidtsing.net.other;
+
+/**
+ * Created by Administrator on 2017/11/17.
+ */
+
+public class Address {
+    public String  ip;
+    public int     port;
+
+    public Address(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+}
