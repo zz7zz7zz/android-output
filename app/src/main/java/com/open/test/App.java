5309b64d305fa744e917fb160ffb6ac853294480
Long <longhui.yang@luzcorp.com>
2021-05-09 21:00:25 +0800

    dokit 测试

--- app/src/main/java/com/open/test/App.java
+++ app/src/main/java/com/open/test/App.java
@@ -2,6 +2,7 @@ package com.open.test;
 
 import android.app.Application;
 
+import com.didichuxing.doraemonkit.DoraemonKit;
 import com.github.moduth.blockcanary.BlockCanary;
 import com.github.moduth.blockcanary.BlockCanaryContext;
 
@@ -10,6 +11,9 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        DoraemonKit.install(this,"88d7426976c87339f80cef833dd4561c");
+
 
 //        BlockCanary.install(this, new BlockCanaryContext(){
 //            @Override
